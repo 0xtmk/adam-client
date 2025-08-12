567b8b2f6cf2c6f445597ee7c4a80a141d0179fa
kienvm <kienvm@moonlab.asia>
2025-08-13 00:24:41 +0700

    Refactor leaderboard page layout by removing the table and replacing it with a descriptive container

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -18,41 +18,11 @@ export const LeaderboardPage: FC<LeaderboardPageProps> = () => {
         </div>
       </Container>
 
-      <div className="bg-fill mx-auto mt-10 w-full max-w-[1440px] bg-[url('/images/leaderboard-card.png')] bg-no-repeat p-10">
-        <div className="max-h-[1514px] w-full overflow-auto">
-          <table className="font-neueMachinaBold w-full text-lg text-white">
-            <thead>
-              <tr className="rounded-t-2xl bg-gradient-to-b from-[#59A1FF] to-[#3E86E4]">
-                <th className="px-4 py-4 text-left">Rank</th>
-                <th className="px-4 py-4 text-left">Name</th>
-                <th className="px-4 py-4 text-center">Total Points</th>
-                <th className="px-4 py-4 text-center">Invite</th>
-              </tr>
-              <tr className="bg-[rgba(89,161,255,0.2)]">
-                <td className="px-4 py-2 text-center">-</td>
-                <td className="px-4 py-2 text-left">User</td>
-                <td className="px-4 py-2 text-center">1</td>
-                <td className="px-4 py-2 text-center">1</td>
-              </tr>
-            </thead>
-            <tbody>
-              {Array.from({ length: 200 }, (_, idx) => idx + 1).map((rank, idx) => (
-                <tr key={rank} className={`$${idx < 3 ? "" : "bg-[rgba(255,255,255,0.04)]"} rounded-2xl`}>
-                  <td className="px-4 py-3 text-center">
-                    {idx === 0 && <img src="/icons/rank-1.png" alt="1" className="inline-block h-8 w-8" />}
-                    {idx === 1 && <img src="/icons/rank-2.png" alt="2" className="inline-block h-8 w-8" />}
-                    {idx === 2 && <img src="/icons/rank-3.png" alt="3" className="inline-block h-8 w-8" />}
-                    {idx > 2 && <span className="font-bold">{rank}</span>}
-                  </td>
-                  <td className="px-4 py-3 text-left">Ynhsfb...ahsdjk</td>
-                  <td className="px-4 py-3 text-center">100</td>
-                  <td className="px-4 py-3 text-center">100</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <Container size="retina" className="card-leaderboard mt-10 h-fit rounded-t-3xl bg-[rgba(21,81,158,0.2)]">
+          <div className="w-[10px] p-10">
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, temporibus explicabo quisquam provident ratione alias possimus velit placeat tenetur. Qui repudiandae obcaecati odit sint magni non, perferendis architecto culpa expedita!
+          </div>
+      </Container>
     </div>
   )
 }
