d6008952346fa256929df00607fea17b68930fc1
kienvm <kienvm@moonlab.asia>
2025-08-21 16:56:44 +0700

    feat: update Solana contracts for USDC and wallet signature in production; add adamIdlMainnet configuration

--- src/libs/web3/solana/constants/solana-contracts.ts
+++ src/libs/web3/solana/constants/solana-contracts.ts
@@ -10,12 +10,12 @@ export const SOLANA_CONTRACTS = {
   USDC: {
     [Env.development]: "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr",
     [Env.staging]: "",
-    [Env.production]: "",
+    [Env.production]: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
   },
 
   WALLET_SIGNATURE: {
     [Env.development]: "Ea49624c767C3c52d79f4D99db0f06E4B5855E28",
     [Env.staging]: "",
-    [Env.production]: "",
+    [Env.production]: "c522141625A0C43461c4BfB449bceb8008d4a2c4",
   },
 }
